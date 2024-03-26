1350c5831ab6ca73cf0dd0c358a4b9fadccaf21a
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2024-03-25 20:25:17 -0400

    add insertion tests

--- data_structs/binary_tree.cpp
+++ data_structs/binary_tree.cpp
@@ -1,5 +1,8 @@
 #include <cassert>
 #include <iostream>
+#include <vector>
+
+using std::vector;
 
 template<typename T>
 struct TreeNode {
@@ -26,7 +29,7 @@ public:
 
     bool insert(T val) {
         if (!root) {
-            root = new TreeNode{val, nullptr, nullptr};
+            root = new TreeNode<T>{val, nullptr, nullptr};
             tree_size ++;
             return true;
         }
@@ -51,14 +54,14 @@ private:
     bool tree_insert(TreeNode<T>* node, T val) {
         if (val > node->val) {
             if (!node->right) {
-                node->right = new TreeNode{val, nullptr, nullptr};
+                node->right = new TreeNode<T>{val, nullptr, nullptr};
                 tree_size ++;
                 return true;
             }
             return tree_insert(node->right, val);
         } else if (val < node->val) {
             if (!node->left) {
-                node->left = new TreeNode{val, nullptr, nullptr};
+                node->left = new TreeNode<T>{val, nullptr, nullptr};
                 tree_size ++;
                 return true;
             }
@@ -112,7 +115,7 @@ private:
         next->right = node->right;
         delete node;
         node = next;
-        
+
         tree_size --;
         return true;
     }
@@ -120,9 +123,61 @@ private:
     friend void test_binary_tree();
 };
 
+template<typename T>
+void check_tree_ordering(TreeNode<T>* node) {
+    if (!node) {
+        return;
+    }
+    if (node->left) {
+        assert(node->left->val < node->val);
+        check_tree_ordering(node->left);
+    }
+    if (node->right) {
+        assert(node->right->val > node->val);
+        check_tree_ordering(node->right);
+    }
+}
+
+template<typename T>
+void check_tree_includes(const BinaryTree<T>& tree, const vector<T>& vals) {
+    for (T val : vals) {
+        assert(tree.find(val));
+    }
+}
+
+template<typename T>
+void check_tree_excludes(const BinaryTree<T>& tree, const vector<T>& vals) {
+    for (T val : vals) {
+        assert(!tree.find(val));
+    }
+}
+
 void test_binary_tree() {
     BinaryTree<int> tree;
-    std::cout << tree.tree_size << "\n";
+    assert(tree.size() == 0);
+    check_tree_excludes(tree, {6, 2, 9, 3, 7, 4, 10, 0, 15});
+    assert(tree.root == nullptr);
+
+    tree.insert(6);
+    assert(tree.size() == 1);
+    check_tree_includes(tree, {6});
+    check_tree_excludes(tree, {2, 9, 3, 7, 4, 10, 0, 15});
+
+    tree.insert(2);
+    tree.insert(9);
+    assert(tree.size() == 3);
+    check_tree_includes(tree, {6, 2, 9});
+    check_tree_excludes(tree, {3, 7, 4, 10, 0, 15});
+    check_tree_ordering(tree.root);
+
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(4);
+    tree.insert(10);
+    assert(tree.size() == 7);
+    check_tree_includes(tree, {6, 2, 9, 3, 7, 4, 10});
+    check_tree_excludes(tree, {0, 15});
+    check_tree_ordering(tree.root);
 }
 
 int main() {
